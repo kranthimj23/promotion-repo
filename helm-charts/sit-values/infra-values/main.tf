7af33f942fc3a507a91c0dae8f7c30ff58c9c117
kranthimj23 <kranthimj23@gmail.com>
2025-07-25 11:24:35 +0530

    remove gke

--- helm-charts/sit-values/infra-values/main.tf
+++ helm-charts/sit-values/infra-values/main.tf
@@ -1,9 +1,9 @@
-module "gke" {
-  source       = "./modules/gke"
-  project_id   = var.project_id
-  region       = var.region
-  cluster_name = var.cluster_name
-}
+# module "gke" {
+#   source       = "./modules/gke"
+#   project_id   = var.project_id
+#   region       = var.region
+#   cluster_name = var.cluster_name
+# }
 
 module "cloudsql" {
   source       = "./modules/cloudsql"
